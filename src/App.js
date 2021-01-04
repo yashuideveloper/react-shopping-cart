2ed2dd52e12c96546563ee62d5cccaba01193f54
yashavanth.kumar <yashavanth.kumar@medibuddy.in>
2021-01-04 16:21:42 +0530

    add product list ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
+import React from 'react';
+import Products from './components/Products';
+import data from './data.json'
 //feature-1
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="">React Shopping Cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All right reserved.
-      </footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products Products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All right reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import formatCurrency from '../utils'
+export default class Products extends Component {
+  render() {
+    return (
+      <div>
+          <ul className="products">
+            {this.props.Products.map(product => (
+              <li key={product._id}>
+                <div className="product">
+                  <a href={"#" + product._id}>
+                    <img src={product.image} alt={product.title}></img>
+                    <p>
+                      {product.title}
+                    </p>
+                  </a>
+                  <div className="product-price">
+                    <div>
+                      {formatCurrency(product.price)}
+                    </div>
+                    <button className="btn-primary">
+                        Add to Cart
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+      </div>
+    )
+  }
+}
